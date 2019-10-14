c8e049deb9b1e271a24491084726d25f46fdaab3
Samuel Bowman <samuelpbowman@gmail.com>
2019-10-14 15:19:53 -0400

    moved data to another file

---
+++ src/la-pa.js
@@ -0,0 +1,216 @@
+const info = {
+    "categories": {
+        "antojitos": [
+            {
+                "name": "Nachos Fajita",
+                "desc": "Tortilla chips topped with grilled bell peppers and onions, covered with cheese dip, lettuce and pico de gallo.",
+                "price": "10.25"
+            },
+            {
+                "name": "Nachos Mexicanos",
+                "desc": "Tortilla chips covered with cheese dip, topped with refried beans, lettuce, tomatoes, guacamole, sour cream, and shredded cheese.",
+                "price": "10.25"
+            },
+            {
+                "name": "Nachos Cheese",
+                "desc": "Tortilla chips covered with cheese dip.",
+                "price": "5.50"
+            },
+            {
+                "name": "Queso Fundido",
+                "desc": "A blend of our house cheeses melted on a roasted poblano peppers and chorizo. Served with tortillas.",
+                "price": "7.50"
+            }
+        ],
+        "tacos": [
+            {
+                "name": "Tacos Mexicanos",
+                "desc": "Soft corn tortillas with choice of meat, fresh chopped onions and cilantro. Served with a side of our tomatillo chipotle sauce.",
+                "price": "2.25"
+            },
+            {
+                "name": "Fish Tacos (3)",
+                "desc": "Three soft flour tortillas filled with beer battered tilapia strips, topped with shredded cabbage, cilantro and our CHORIZO house zesty sauce. Served with side of black beans.",
+                "price": "9.99"
+            },
+            {
+                "name": "April's Tacos",
+                "desc": "Two soft (flour) tacos with shredded chicken, lettuce & tomatoes. Served with a side of black beans.",
+                "price": "6.50"
+            },
+            {
+                "name": "Shrimp Tacos (3)",
+                "desc": "Three street style-grilled corn tortil as with grilled spicy shrimp, shredded cabbage, cilantro, pico de gallo and sour cream drizzle. Served with a side of black beans.",
+                "price": "9.99"
+            }
+        ],
+        "enchiladas": [
+            {
+                "name": "Enchiladas Mexicanas",
+                "desc": "Four corn tortillas stuffed with your choice Of meat & filling, hand rolled and covered with cheese dip. Topped with lettuce, tomatoes, sour cream, onions and shredded cheese.",
+                "price": "10.75"
+            },
+            {
+                "name": "Enchiladas Rojas",
+                "desc": "Four corn tortillas stuffed with your choice of meat of filling, hand rolled and covered with our red \"enchilada sauce\". Topped with lettuce, tomatoes, sour cream, onions and shredded cheese.",
+                "price": "9.99"
+            },
+            {
+                "name": "Enchiladas Verdes",
+                "desc": "Four corn tortillas stuffed with your choice of meat of filling, hand rolled and covered with salsa verde. Topped with lettuce tomatoes, sour cream, onions and shredded cheese.",
+                "price": "9.99"
+            }
+        ],
+        "flautas": [
+            {
+                "name": "Flautas Supremas",
+                "desc": "Three corn tortillas filled with either shredded beef or chicken, rolled and deep fried. Topped with lettuce, tomatoes, sour cream and shredded cheese. Served with rice and refried beans.",
+                "price": "10.75"
+            },
+            {
+                "name": "Flautas Dinner",
+                "desc": "Four corn tortillas filled with either shredded beef or chicken, rolled and deep fried. Served with rice, refried beans and guacamole salad.",
+                "price": "10.75"
+            }
+        ],
+        "burritos": [
+            {
+                "name": "Burrito de Carne Asada",
+                "desc": "Flour tortilla (12\") stuffed with your choice Of meat, rice, grilled onions, tomatoes and bell peppers. Served with lettuce, sour cream and pico de gallo.",
+                "price": "9.25"
+            },
+            {
+                "name": "Burrito de Pescado",
+                "desc": "Flour tortilla la (12\") filled with beer battered tilapia strips, rice, black beans, pico de gallo, shredded cabbage and Our house zesty sauce. ",
+                "price": "7.50"
+            },
+            {
+                "name": "Burrito Vallarta",
+                "desc": "Flour tortilla (12\") stuffed with rice, jalapeños, grilled shrimp and sautéed veggies (bell peppers, onions, zucchini, tomatoes and mushrooms). Served with lettuce, sour cream and pico de gallo.",
+                "price": "9.50"
+            },
+            {
+                "name": "Burrito Especial",
+                "desc": "Flour tortilla (12\") stuffed with your choice of meat, refried beans, lettuce, tomatoes, onions, sour cream and jalapeños. ",
+                "price": "6.00"
+            },
+            {
+                "name": "Burrito Vegetariano",
+                "desc": "Flour tortilla/ 12\") stuffed with rice, refried beans, lettuce, tomatoes, onions, sour cream, shredded cheese and jalapeños. Flour tortilla (8\") filled with your choice of meat or filling. Topped with shredded cheese and our house tomato sauce.",
+                "price": "4.00"
+            },
+            {
+                "name": "Burrito Supremo",
+                "desc": "Flour tortilla (8\") filled with your choice of meat or filling. Covered with our house tomato sauce, topped with lettuce, tomatoes, sour creamand shredded cheese.",
+                "price": "7.50"
+            }
+        ],
+        "ensaladas": [
+            {
+                "name": "Taco Salad",
+                "desc": "Crispy tortilla shell filled with refried beans and your choice of meat. Topped with shredded cheese, lettuce, tomatoes, guacamole and sour cream.",
+                "price": "7.99"
+            },
+            {
+                "name": "Fajita Taco Salad",
+                "desc": "Crispy tortilla shell filled with refried beans and your choice of meat. Topped with grilled bell peppers and onions, lettuce, tomatoes, sour cream, guacamole and shredded cheese.",
+                "price": "8.99"
+            },
+            {
+                "name": "Veggie Taco Salad",
+                "desc": "Crispy tortilla shell filled with refried beans, rice, shredded cheese, lettuce, tomatoes, sour cream and guacamole.",
+                "price": "5.99"
+            },
+            {
+                "name": "Guacamole Salad",
+                "desc": "Small crispy tortilla shell filled with lettuce, tomatoes and guacamole.",
+                "price": "2.99"
+            },
+            {
+                "name": "Baked Fajita Taco Salad",
+                "desc": "Baked tortilla shell filled with black beans and your choice of meat. Topped with grilled bell peppers and onions, lettuce, tomatoes and chunky guacamole.",
+                "price": "8.99"
+            }
+        ],
+        "quesadillas": [
+            {
+                "name": "Quesadilla Fajita",
+                "desc": "A folded flour tortilla filled with your choice of meat and melted cheese, grilled onions, bell peppers and tomatoes.",
+                "price": "5.75"
+            },
+            {
+                "name": "Quesadilla Chicken",
+                "desc": "A cheesy favorite made with our popular shredded chicken.",
+                "price": "4.75"
+            },
+            {
+                "name": "Quesadilla Veggie",
+                "desc": "Sauteed onions, bell peppers, tomatoes, zucchini, pico, mushrooms and melted cheese.",
+                "price": "4.25"
+            },
+            {
+                "name": "Quesadilla Cheese",
+                "desc": "A folded Flour tortilla filled with melted cheese.",
+                "price": "3.50"
+            },
+            {
+                "name": "Quesadilla Fajita de Shrimp",
+                "desc": "Shrimp grilled with bell peppers, onions and tomatoes.",
+                "price": "6.25"
+            }
+        ],
+        "fajitas": [
+            {
+                "name": "Fajitas (Steak or Chicken)",
+                "desc": "Seasoned fajita style chicken or steak strips, grilled with bell peppers and onions. Served on a hot skillet.",
+                "price": "11.75"
+            },
+            {
+                "name": "Fajitas Mix",
+                "desc": "A combination of chicken and steak strips, grilled with bell peppers and onions. Serve ona hot skillet.",
+                "price": "12.50"
+            },
+            {
+                "name": "Fajitas Shrimp",
+                "desc": "Fajita seasoned shrimp grilled with bell peppers and onions. Served on a hot skillet.",
+                "price": "12.99"
+            },
+            {
+                "name": "Fajitas Tapatias",
+                "desc": "A combination of chorizo, chicken and steak strips, grilled with bell peppers and onions. Served on a hot skillet.",
+                "price": "13.50"
+            },
+            {
+                "name": "Fajitas Veggie",
+                "desc": "Grilled onions, bell peppers, tomatoes, zucchini and mushrooms. Served on a hotskillet.",
+                "price": "9.99"
+            }
+        ],
+        "tortas": [
+            {
+                "name": "Coming Soon",
+                "desc": "We are always adding more things!",
+                "price": "0"
+            }
+        ],
+        "tomadas": [
+            {
+                "name": "Coke Products",
+                "desc": "Choose from Coke, Sprite, or one of many other Coca-Cola products",
+                "price": "2.69"
+            },
+            {
+                "name": "Domestics",
+                "desc": "Consumable on premises only. Budweiser, Bud Light, Miller High Life, Miller Lite",
+                "price": "4"
+            },
+            {
+                "name": "Cerveza",
+                "desc": "Modelo Especial, Modelo Negro, Corona Extra, Corona Light, Tecate, Tecate Blue",
+                "price": "5"
+            }
+        ]
+    }
+};
+
+export default info;
